047849ab640ec544638b3e44c25cdaf61de66e8b
layan101 <159005114+layan101@users.noreply.github.com>
2024-02-05 10:04:52 +0100

    Add files via upload
    Info-Lk-Packages

---
+++ Info-LK/src/info_lk/datenbanken/mini_projekte/Baecker.java
@@ -0,0 +1,17 @@
+package info_lk.datenbanken.mini_projekte;
+
+public class Baecker {
+
+    /* Statische Variablen */
+
+    /* Statische Methoden */
+
+    /* Attribute */
+
+    /* Konstruktoren */
+
+    /* Objektmethoden */
+
+    /* Getter und Setter */
+
+}
---
+++ Info-LK/src/info_lk/datenbanken/mini_projekte/Kuchen.java
@@ -0,0 +1,17 @@
+package info_lk.datenbanken.mini_projekte;
+
+public class Kuchen {
+
+    /* Statische Variablen */
+
+    /* Statische Methoden */
+
+    /* Attribute */
+
+    /* Konstruktoren */
+
+    /* Objektmethoden */
+
+    /* Getter und Setter */
+
+}
